dae67e327eae30d081766babb9bacefc2ca28bcd
h4rry5lol <harasta.ivan@gmail.com>
2017-04-03 20:32:50 +0200

    minor change - remove user-color class from logo div

--- other/logo.php
+++ other/logo.php
@@ -1,5 +1,5 @@
 <div class='w3-row'>
-    <div class='w3-col l12 w3-padding-large w3-border-bottom w3-center w3-black<?php //echo $_SESSION['user']->color; ?> w3-border-black w3-bottombar logo'>
+    <div class='w3-col l12 w3-padding-large w3-border-bottom w3-center w3-black w3-border-black w3-bottombar logo'>
         <h1><a href="index.php">FineFriend</a></h1>
     </div>
 </div>
