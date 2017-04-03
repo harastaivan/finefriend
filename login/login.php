690dd4251169206463c8516222dd9d9c4d1570a9
h4rry5lol <harasta.ivan@gmail.com>
2017-04-03 20:38:47 +0200

    login & register php code review

--- login/login.php
+++ login/login.php
@@ -1,8 +1,6 @@
 <?php
 
-
-    require('register-form.php');
-    require('login-form.php');
-
+require('register-form.php');
+require('login-form.php');
 
 ?>
--- login/validate-register.php
+++ login/validate-register.php
@@ -1,57 +1,54 @@
 <?php 
 
-    if ( isset($_POST['register']) ) {
-        $name = $_POST['name'];
-        $surname = $_POST['surname'];
-        $email = $_POST['email'];
-        $pw1 = $_POST['password1'];
-        $pw2 = $_POST['password2'];
-        $pwHash = password_hash($pw1, PASSWORD_DEFAULT);
-        $img = $_POST['image'];
-        $color = $_POST['color'];
-        $time = time();
-        
-        //shoduji se hesla
-        if ( $pw1 == $pw2 ) {
-            $query = "SELECT email FROM users WHERE email LIKE '$email'";
+if ( isset($_POST['register']) ) {
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $pw1 = $_POST['password1'];
+    $pw2 = $_POST['password2'];
+    $pwHash = password_hash($pw1, PASSWORD_DEFAULT);
+    $img = $_POST['image'];
+    $color = $_POST['color'];
+    $time = time();
+
+    //shoduji se hesla
+    if ( $pw1 == $pw2 ) {
+        $query = "SELECT email FROM users WHERE email LIKE '$email'";
+        $result = $conn->query($query);
+
+        $rowcount = mysqli_num_rows($result);
+
+        if ( $rowcount == 0 ) {
+
+            if (empty($img) || $img == '') {
+                $img = 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png';
+            }
+
+            $query =    "INSERT INTO users (name, surname, email, password, profilepic, color, timestamp) 
+                        VALUES ('$name', '$surname', '$email', '$pwHash', '$img', '$color', $time)";
+
             $result = $conn->query($query);
-            
-            $rowcount = mysqli_num_rows($result);
-            
-            if ( $rowcount == 0 ) {
-                
-                if (empty($img) || $img == '') {
-                    $img = 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png';
-                }
-            
-                $query =    "INSERT INTO users (name, surname, email, password, profilepic, color, timestamp) 
-                            VALUES ('$name', '$surname', '$email', '$pwHash', '$img', '$color', $time)";
 
-                $result = $conn->query($query);
-
-                if ($result) {
-                    $msg = 'You are successfully registered!';
-                    $pre_msg = "<div class='w3-panel w3-green w3-card-8 alertpanel'>";
-                } else {
-                    $msg = 'Try again :(';
-                    $pre_msg = "<div class='w3-panel w3-yellow w3-card-8 alertpanel'>";
-                }
+            if ($result) {
+                $msg = 'You are successfully registered!';
+                $pre_msg = "<div class='w3-panel w3-green w3-card-8 alertpanel'>";
             } else {
-                $msg = 'Email already in use :(';
+                $msg = 'Try again :(';
                 $pre_msg = "<div class='w3-panel w3-yellow w3-card-8 alertpanel'>";
             }
-      
         } else {
-            $msg = "Passwords don't match :(";
-            $pre_msg = "<div class='w3-panel w3-red w3-card-8 alertpanel'>";
+            $msg = 'Email already in use :(';
+            $pre_msg = "<div class='w3-panel w3-yellow w3-card-8 alertpanel'>";
         }
-        echo $pre_msg;
-        echo $msg;
-        echo '</div>';
-        
-        //echo "<script>setTimeout(function(){location.href='index.php'}, 4000);</script>";
-        
-    }
 
+    } else {
+        $msg = "Passwords don't match :(";
+        $pre_msg = "<div class='w3-panel w3-red w3-card-8 alertpanel'>";
+    }
+    echo $pre_msg;
+    echo $msg;
+    echo '</div>';
+
+}
 
 ?>
