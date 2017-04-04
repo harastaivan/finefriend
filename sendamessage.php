4ed716f6886eea32540dd35f44c3c4cf612534bc
Ivan Harasta <harasta.ivan@gmail.com>
2017-04-04 18:24:42 +0200

    right page after sending a message

--- sendamessage.php
+++ sendamessage.php
@@ -1,7 +1,7 @@
 <?php
 
 $result = sendAMessage($_GET['who'], $_GET['whom'], $_GET['text']);
-echo "<script>location.href=`index.php?feed=1`</script>";
+echo "<script>location.href=`index.php" . $_SESSION['page'] . "`</script>";
 
 
 ?>
