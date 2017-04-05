814457c81645a3c603ea39244b4b124057a2d427
Ivan Harasta <harasta.ivan@gmail.com>
2017-04-05 15:15:42 +0200

    minor css change

--- other/menu.php
+++ other/menu.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "  <div class='w3-row w3-hide-large w3-border-bottom w3-border-black w3-bottombar' id='menu'>
-            <div class='w3-col s4 m4 w3-padding-small w3-center w3-" . $_SESSION['user']->color . "'>
+            <div class='w3-col s4 m4 w3-padding-small w3-center w3-" . $_SESSION['user']->color . " gradient'>
                 <h3><a onclick='showProfileTab()'>Profile</a></h3>
             </div>
             <div class='w3-col s4 m4 w3-padding-small w3-center darker-gray w3-text-white'>
