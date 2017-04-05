be0773c61af263da4a3c7ffd8ec90712cae62802
Ivan Harasta <harasta.ivan@gmail.com>
2017-04-05 16:04:32 +0200

    fixed message show

--- feed.php
+++ feed.php
@@ -32,7 +32,7 @@ foreach( $feed as $status ) {
     }
 
     echo "  <div class='padding-medium margin-top-medium w3-$thisUser->color gradient'>
-                <div class='w3-row time-parent'>
+                <div class='w3-row relative-parent'>
                     <div class='w3-col' style='width:100px'>
                         <div class='avatar' style='background-image:url(" . $thisUser->image . ")'></div>
                     </div>
