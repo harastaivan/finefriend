1d72561509e98b87e59480818cac72a0d2c5dba4
Ivan Harasta <harasta.ivan@gmail.com>
2017-04-08 13:54:52 +0200

    character set fix from latin1 to utf8

--- other/connection.php
+++ other/connection.php
@@ -10,5 +10,13 @@ if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
 
+//echo "Initial character set: " . $conn->character_set_name();
+
+if (!$conn->set_charset("utf8")) {
+    die('Error loading character set utf8: ' . $conn->error);
+} else {
+    //echo "Current character set: " . $conn->character_set_name();
+}
+
 
 ?>
