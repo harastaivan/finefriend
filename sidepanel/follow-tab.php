ebff0f749d4298aa68d15a338f03327be7d8da3e
Ivan Harasta <harasta.ivan@gmail.com>
2017-04-09 20:55:40 +0200

    show only 5 users to follow in feed

--- sidepanel/follow-tab.php
+++ sidepanel/follow-tab.php
@@ -5,9 +5,19 @@ echo "  <div class='padding-horizontal-medium whoToFollow'>
 
 $users = selectAllUsers();
 
+if ($_SESSION["page"] != "?whoToFollow=1") {
+    $howMuch = 5;
+}
+
 foreach ($users as $user) {
     if( $user->id !== $_SESSION["user"]->id && !$_SESSION["user"]->doIFollowHim($user)) {
         showUser($user, $_SESSION['page']);
+        if (isset($howMuch)) {
+            $howMuch--;
+            if ($howMuch <= 0) {
+                break;
+            }
+        }
     }
 }
 
