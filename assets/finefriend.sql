a12537a7c61c8cad26c29f6abb7ae72b76b2f327
Ivan Harasta <harasta.ivan@gmail.com>
2017-04-09 22:30:42 +0200

    upload updated database

--- assets/finefriend.sql
+++ assets/finefriend.sql
@@ -1,14 +1,11 @@
-SET NAMES 'utf8';
-SET CHARSET 'utf8';
-
 -- phpMyAdmin SQL Dump
--- version 4.4.4
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Počítač: sql.endora.cz:3308
--- Vytvořeno: Pát 07. dub 2017, 20:19
--- Verze serveru: 5.6.28-76.1
--- Verze PHP: 5.4.45
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Ned 09. dub 2017, 22:29
+-- Verze serveru: 10.1.16-MariaDB
+-- Verze PHP: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Databáze: `finefriend`
@@ -29,12 +26,12 @@ SET time_zone = "+00:00";
 -- Struktura tabulky `feed`
 --
 
-CREATE TABLE IF NOT EXISTS `feed` (
+CREATE TABLE `feed` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `text` varchar(200) NOT NULL,
   `timestamp` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Vypisuji data pro tabulku `feed`
@@ -77,7 +74,13 @@ INSERT INTO `feed` (`id`, `user_id`, `text`, `timestamp`) VALUES
 (35, 34, 'ZULUL', 1491463766),
 (36, 56, 'Reeeeeeeeee', 1491464985),
 (37, 56, 'REEEEEEEEEEEEEE', 1491465130),
-(38, 42, 'Co je ty píčo', 1491465185);
+(38, 42, 'Co je ty píčo', 1491465185),
+(39, 34, 'Už to funguje NICE', 1491652547),
+(40, 57, 'Test', 1491761383),
+(41, 34, 'pls', 1491765591),
+(42, 34, 'pls', 1491765675),
+(43, 34, 'haha', 1491765681),
+(44, 34, 'zulul', 1491765741);
 
 -- --------------------------------------------------------
 
@@ -85,12 +88,12 @@ INSERT INTO `feed` (`id`, `user_id`, `text`, `timestamp`) VALUES
 -- Struktura tabulky `follows`
 --
 
-CREATE TABLE IF NOT EXISTS `follows` (
+CREATE TABLE `follows` (
   `id` int(11) NOT NULL,
   `follower_id` int(11) NOT NULL,
   `followed_id` int(11) NOT NULL,
   `timestamp` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=97 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Vypisuji data pro tabulku `follows`
@@ -129,7 +132,7 @@ INSERT INTO `follows` (`id`, `follower_id`, `followed_id`, `timestamp`) VALUES
 (37, 36, 36, 0),
 (38, 42, 33, 0),
 (39, 42, 42, 0),
-(61, 34, 48, 0),
+(119, 34, 57, 1491767822),
 (41, 42, 34, 0),
 (45, 35, 35, 0),
 (46, 33, 46, 0),
@@ -147,7 +150,6 @@ INSERT INTO `follows` (`id`, `follower_id`, `followed_id`, `timestamp`) VALUES
 (59, 48, 44, 0),
 (60, 48, 41, 0),
 (67, 51, 34, 0),
-(68, 34, 51, 0),
 (73, 52, 34, 0),
 (71, 51, 33, 0),
 (72, 51, 37, 0),
@@ -158,7 +160,7 @@ INSERT INTO `follows` (`id`, `follower_id`, `followed_id`, `timestamp`) VALUES
 (78, 55, 33, 1491408328),
 (79, 55, 32, 1491408330),
 (80, 55, 44, 1491408337),
-(81, 34, 38, 1491413760),
+(97, 57, 57, 1491761079),
 (82, 34, 52, 1491413765),
 (83, 34, 33, 1491464414),
 (84, 34, 44, 1491464427),
@@ -172,7 +174,28 @@ INSERT INTO `follows` (`id`, `follower_id`, `followed_id`, `timestamp`) VALUES
 (92, 42, 56, 1491465087),
 (93, 56, 42, 1491465103),
 (94, 34, 56, 1491465118),
-(96, 36, 44, 1491582153);
+(96, 36, 44, 1491582153),
+(98, 57, 56, 1491761857),
+(99, 57, 49, 1491764003),
+(100, 57, 54, 1491764006),
+(101, 57, 52, 1491764008),
+(102, 57, 48, 1491764011),
+(103, 57, 51, 1491764018),
+(104, 57, 50, 1491764020),
+(105, 57, 46, 1491764021),
+(106, 57, 44, 1491764023),
+(107, 57, 39, 1491764026),
+(108, 57, 37, 1491764028),
+(109, 57, 38, 1491764030),
+(110, 57, 40, 1491764032),
+(111, 57, 36, 1491764033),
+(112, 57, 34, 1491764034),
+(113, 57, 32, 1491764039),
+(114, 57, 42, 1491764041),
+(115, 57, 31, 1491764044),
+(116, 57, 33, 1491764045),
+(117, 57, 41, 1491764046),
+(118, 57, 35, 1491764047);
 
 -- --------------------------------------------------------
 
@@ -180,7 +203,7 @@ INSERT INTO `follows` (`id`, `follower_id`, `followed_id`, `timestamp`) VALUES
 -- Struktura tabulky `messages`
 --
 
-CREATE TABLE IF NOT EXISTS `messages` (
+CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
   `sender_id` int(11) NOT NULL,
   `recipient_id` int(11) NOT NULL,
@@ -193,65 +216,11 @@ CREATE TABLE IF NOT EXISTS `messages` (
 --
 
 INSERT INTO `messages` (`id`, `sender_id`, `recipient_id`, `text`, `timestamp`) VALUES
-(0, 34, 38, 'A-a-a-a-hoj T-t-tome!', 1490713370),
-(0, 38, 34, 'Co je kurva?', 1490713389),
-(0, 34, 35, 'Bejre!', 1490713430),
-(0, 35, 34, 'JEBEEEEJ', 1490713458),
-(0, 46, 33, 'hey', 1490724348),
-(0, 46, 39, 'mrdko harry to docela zabiji', 1490724373),
-(0, 33, 46, 'hou', 1490724453),
-(0, 33, 34, 'ty jsi zabiják\r\n', 1490724599),
-(0, 33, 34, 'gl tyvole, dobre ty:D nerde:DD', 1490724646),
-(0, 33, 34, 'gl tyvole, dobre ty:D nerde:DD', 1490724909),
-(0, 33, 34, 'gl tyvole, dobre ty:D nerde:DD', 1490727515),
-(0, 36, 34, 'motůrky :D\r\n', 1490784769),
-(0, 34, 36, '????', 1490784792),
-(0, 36, 34, 'budem programovat za týden :P\r\n', 1490784813),
-(0, 36, 38, 'wanna rob a bank?', 1490784922),
-(0, 36, 34, 'zkousim enter\r\nhusty!', 1490784972),
-(0, 48, 33, 'čus morys\r\n', 1490784986),
-(0, 36, 34, 'nefunguje, uplne k nicemu :D', 1490784987),
-(0, 33, 48, 'cus more\r\n', 1490785004),
-(0, 48, 33, 'šalalalaj čabajku k veceri dones\r\n', 1490785017),
-(0, 34, 36, 'je to textarea takze to nefunguje, asi misto toho dam normal input', 1490785341),
-(0, 34, 35, 'pls', 1490786637),
-(0, 34, 35, 'pls', 1490786638),
-(0, 50, 33, 'Hej ty zmrde', 1491145144),
-(0, 51, 34, 'Jsi gay ', 1491321334),
-(0, 34, 51, 'Ahoj\r\n', 1491321387),
-(0, 51, 34, 'Jsi gay ', 1491321391),
-(0, 51, 34, 'Lol', 1491321405),
-(0, 51, 34, 'Co deláš ', 1491321436),
-(0, 34, 51, 'Sukam se sushim', 1491321447),
-(0, 34, 51, 'Sukam se sushim', 1491321453),
-(0, 51, 34, 'Taky taky ', 1491321789),
-(0, 51, 34, 'Taky taky ', 1491322047),
-(0, 51, 44, 'Didopici', 1491322067),
-(0, 51, 31, 'Shksndbd', 1491323139),
-(0, 52, 34, 'Dobry den', 1491324107),
-(0, 51, 52, 'Nazdar ', 1491324281),
-(0, 51, 34, 'My uz nejsme kamarádi ?', 1491324297),
-(0, 51, 53, 'Medko', 1491324309),
-(0, 52, 51, 'Dobry den', 1491324312),
-(0, 51, 53, 'Medko', 1491324467),
-(0, 51, 52, 'Funguje to huraaa', 1491324498),
-(0, 52, 51, 'Juu', 1491327019),
-(0, 34, 36, 'Tak co dobrý', 1491387707),
-(0, 51, 52, 'Send nudes', 1491393358),
-(0, 54, 51, 'SMRDIS MILANE\r\n', 1491393465),
-(0, 51, 54, 'Cozd', 1491393495),
-(0, 34, 52, 'Ahoj', 1491399993),
-(0, 55, 33, 'Hoven', 1491408369),
-(0, 55, 33, 'hovno', 1491408378),
-(0, 34, 35, 'JEBEEEEEEJ', 1491463845),
-(0, 34, 37, 'Ta rozbijem o dvanácté pri zvončekov', 1491464731),
-(0, 42, 56, 'co je ty pico?', 1491465079),
-(0, 56, 49, 'You dont hold the smoke.. \r\nMaybe you dont', 1491465085),
-(0, 34, 56, 'Co je ty píčo?', 1491465154),
-(0, 42, 56, 'kokote', 1491465162),
-(0, 34, 42, 'Co je ty píčo?', 1491465188),
-(0, 36, 34, 'ivane?\r\n', 1491508110),
-(0, 36, 34, '"normální uvozovky jsou ok ne?"', 1491508359);
+(1, 34, 31, 'message to biblethump', 1491745037),
+(2, 57, 38, 'Ahoj Tome', 1491761430),
+(3, 57, 33, 'Ahoj Vine nemám rád diesely', 1491761442),
+(4, 57, 44, 'cau cina', 1491761453),
+(5, 57, 56, 'co je???', 1491763112);
 
 -- --------------------------------------------------------
 
@@ -259,7 +228,7 @@ INSERT INTO `messages` (`id`, `sender_id`, `recipient_id`, `text`, `timestamp`) 
 -- Struktura tabulky `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) COLLATE utf8_czech_ci NOT NULL,
   `surname` varchar(100) COLLATE utf8_czech_ci NOT NULL,
@@ -268,7 +237,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `profilepic` varchar(300) COLLATE utf8_czech_ci NOT NULL,
   `color` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `timestamp` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=57 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
 -- Vypisuji data pro tabulku `users`
@@ -279,23 +248,24 @@ INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `profilepic`,
 (32, 'Kappa', 'Pride', 'kappa@pride', '$2y$10$d/icN54Q7TV22X3rXK9JXucXF2ZwtUqU06YODYi0NgtqrCw8hwSu.', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRwTQIOQKAvq3iS6Oh_vh_CCLCfN8IOc50yhfbw1a9UCLkHjSsEkxVIQQ', 'cyan', 1489399740),
 (31, 'Bible', 'Thump', 'bible@thump', '$2y$10$Y8vQTaqRBD1AvXWhn2TTKOy/PRFyFMUtIDLBRz9HOA6DzZFM8cj0u', 'https://pbs.twimg.com/profile_images/577309793104175104/y2jIo9dq.png', 'pink', 1489399595),
 (38, 'Tom', 'Angelo', 'tom@angelo', '$2y$10$svND79d6rkcen02sVw.rfOpbw369NJfURo5JRaIbp22phuh1iXmrm', 'http://www.postavy.cz/obrazky-thumbs/wiki/thomas-angelo-33227-wikithumb.jpg', 'green', 1490302761),
-(44, 'tomas', 'cink', 'cinktomik@gmail.com', '$2y$10$LXpO2EREedsluFBwiw8WAOpv88qPt4qNt0CW/CTcqoqXPGSKaF.iO', 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png', 'indigo', 1490305367),
-(36, 'Jenny', 'Nick', 'jindrola0@seznam.cz', '$2y$10$x3bdQEfjMw3IkBJWvBR1XOOHBTNzwYrQmTed.5hxqDvPivCB6Z4BK', 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png', 'green', 1490253007),
+(44, 'tomas', 'cink', 'cinktomik@gmail.com', '$2y$10$LXpO2EREedsluFBwiw8WAOpv88qPt4qNt0CW/CTcqoqXPGSKaF.iO', 'assets/default-avatar.jpg', 'indigo', 1490305367),
+(36, 'Jenny', 'Nick', 'jindrola0@seznam.cz', '$2y$10$x3bdQEfjMw3IkBJWvBR1XOOHBTNzwYrQmTed.5hxqDvPivCB6Z4BK', 'assets/default-avatar.jpg', 'green', 1490253007),
 (37, 'MARINAK', ' ', 'marinak@marinak', '$2y$10$8hDbnH7RaeE2EppHcZURaeBQ7Kv1SZrXfmVEpr1g0pewpNaXrwTVa', 'http://img1.netky.sk/VIDEO-Marinaka-urazil-cesky-bulvar-Odkazal-im-toto.jpg?imageId=1178160', 'indigo', 1490257224),
-(34, 'Ivan', 'Harašta', 'ivan@harasta', '$2y$10$7XazKi7vszKjEpizMfUXIeHiRR/t52641XDJb3vACyixYo1NM2fG2', 'http://files.gamebanana.com/img/ico/sprays/57822c19e1ad1.png', 'green', 1490217871),
+(34, 'Ivan', 'Harašta', 'ivan@harasta', '$2y$10$7XazKi7vszKjEpizMfUXIeHiRR/t52641XDJb3vACyixYo1NM2fG2', 'http://files.gamebanana.com/img/ico/sprays/57822c19e1ad1.png', 'blue', 1490217871),
 (35, 'Aleš', 'Bejr', 'ales@bejr', '$2y$10$ZwQmPWXXJassUDGcbgPbzejIcPJQS8tpycmuURAzrXhjlxkhgMEC6', 'https://i.ytimg.com/vi/c8mhQDb9Qbg/maxresdefault.jpg', 'blue', 1490218327),
 (39, 'Adam', 'Vandam', 'raketa@prujem.com', '$2y$10$fvhP8xnIM9CYB3VGzwMaOeU11eVlfI6rBCbLlywaN7usKOYmElva6', 'https://s-media-cache-ak0.pinimg.com/originals/0e/a2/48/0ea248b9a893581962b223f9713b6182.jpg', 'red', 1490303260),
-(42, 'Tos nevěděl', 'co ty retardo', 'akosed@gmail.com', '$2y$10$jh7D44MoEdaT.vD2rM3Jheyn1XdyYWlz8xIoBzOHz8ZHJYHPRCaTm', 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png', 'red', 1490304326),
+(42, 'Tos nevěděl', 'co ty retardo', 'akosed@gmail.com', '$2y$10$jh7D44MoEdaT.vD2rM3Jheyn1XdyYWlz8xIoBzOHz8ZHJYHPRCaTm', 'assets/default-avatar.jpg', 'red', 1490304326),
 (40, 'Redy', 'W', 'redy@w', '$2y$10$63iZndgQ9mIKKNA.KkAP6.Xh04MuZLldRiWKjI.qBn710IrkChDjO', 'https://i.ytimg.com/vi/qmLHFu5rfoI/hqdefault.jpg', 'orange', 1490304067),
 (41, 'zolik', '22', 'zolo@22', '$2y$10$e2vZ/h03arPl02.B1ZMw/.zwVxULM581altvBoq8oHHorP0gKRvE6', 'https://i.ytimg.com/vi/_8AhESiTKD0/maxresdefault.jpg', 'red', 1490304143),
-(50, 'noskop', 'noskop', 'noskop@noskop', '$2y$10$H3vSzQwUrnsXsgg8w/8C4OXMLcxWQ9Fcht4fT6s0VsUKWtbPWFPSi', 'noskop', 'blue', 1491145094),
-(46, 'Kuli', 'Očko', 'kuliocko@kulikuli.cz', '$2y$10$/SZ.cZmWFzHVunOE6K9ZlOZYP0YaCnptVr4hQDkbTOuBOuHeyTJeO', 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png', 'indigo', 1490724329),
+(50, 'noskop', 'noskop', 'noskop@noskop', '$2y$10$H3vSzQwUrnsXsgg8w/8C4OXMLcxWQ9Fcht4fT6s0VsUKWtbPWFPSi', 'assets/default-avatar.jpg', 'blue', 1491145094),
+(46, 'Kuli', 'Očko', 'kuliocko@kulikuli.cz', '$2y$10$/SZ.cZmWFzHVunOE6K9ZlOZYP0YaCnptVr4hQDkbTOuBOuHeyTJeO', 'assets/default-avatar.jpg', 'indigo', 1490724329),
 (49, 'Matěj', 'Krčín', 'krcin.matt@seznam.cz', '$2y$10$DjgDfMZT6rcO5QhanKZfd.PrSvPDEn/ZhqDEDc265vy.mjRZv/qEq', 'https://i.ytimg.com/vi/evdv6hFrxpw/maxresdefault.jpg', 'red', 1490806448),
 (48, 'Chuck', 'Norris', 'chuck@norris.cz', '$2y$10$q3yIe7edU2ia6szBGbWqy.GQVkkqcPzrW6L.AR6s6AfUeF2gEYn7G', 'http://www.spektra.wz.cz/tabor09/chuck/chuck1.jpg', 'orange', 1490784921),
-(51, 'Jakub', 'Marikos', 'jakub@eda', '$2y$10$2BXcF397qK7sdIbP3DLhb.tSujc/sONkt2QGMo73xiQfUyoJn/qQ2', 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png', 'orange', 1491321272),
-(52, 'Danuska', 'Tranová ', 'danisek39@gmail.com', '$2y$10$tiGnde8el7j97sYrsPGpM.JM94lEWNIjaBlvZpJMZLWx0gNccpqPu', 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png', 'cyan', 1491324016),
+(51, 'Jakub', 'Marikos', 'jakub@eda', '$2y$10$2BXcF397qK7sdIbP3DLhb.tSujc/sONkt2QGMo73xiQfUyoJn/qQ2', 'assets/default-avatar.jpg', 'orange', 1491321272),
+(52, 'Danuska', 'Tranová ', 'danisek39@gmail.com', '$2y$10$tiGnde8el7j97sYrsPGpM.JM94lEWNIjaBlvZpJMZLWx0gNccpqPu', 'assets/default-avatar.jpg', 'cyan', 1491324016),
 (56, 'Normies', 'Begone', 'vladimir.zabransky@seznam.cz', '$2y$10$D9IzBQJQJWPyRD1hwVHGoe.iwuDvbFHvn7tn8qKiXxKW32Z0zsj.W', 'https://i.warosu.org/data/lit/img/0072/75/1445675131472.jpg', 'black', 1491464959),
-(54, 'tomas', 'nguyen', 'ngkikokot1201@seznam.cz', '$2y$10$93b8VsXBuQrzDfpwyAxnX.8MqmYBRGhDz3RLz/mNJ5dMkQSwUYULC', 'https://i.gyazo.com/41fd490efd0ca48c391025113a130a68.png', 'green', 1491393422);
+(54, 'tomas', 'nguyen', 'ngkikokot1201@seznam.cz', '$2y$10$93b8VsXBuQrzDfpwyAxnX.8MqmYBRGhDz3RLz/mNJ5dMkQSwUYULC', 'assets/default-avatar.jpg', 'green', 1491393422),
+(57, 'test', 'estse', 'test@testik', '$2y$10$i1tQtj5JsVrMeewujRo5YeSdwbk1lcMmv5xynzJDidFpyVjk4P4pm', 'assets/default-avatar.jpg', 'green', 1491761079);
 
 --
 -- Klíče pro exportované tabulky
@@ -314,6 +284,12 @@ ALTER TABLE `follows`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Klíče pro tabulku `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Klíče pro tabulku `users`
 --
 ALTER TABLE `users`
@@ -327,17 +303,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pro tabulku `feed`
 --
 ALTER TABLE `feed`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 --
 -- AUTO_INCREMENT pro tabulku `follows`
 --
 ALTER TABLE `follows`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=97;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
+--
+-- AUTO_INCREMENT pro tabulku `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pro tabulku `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
