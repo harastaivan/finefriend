5ed9a891acdcbbeeeacbc1a20fc9390ae9b77b7d
Ivan Harasta <harasta.ivan@gmail.com>
2017-04-18 19:57:28 +0200

    download w3.css

--- other/head.php
+++ other/head.php
@@ -4,8 +4,8 @@
     <meta charset='UTF-8'>
     <title>FineFriend</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link href='https://fonts.googleapis.com/css?family=Pacifico|Roboto|Open+Sans' rel='stylesheet'>
-    <link rel='stylesheet' href='https://www.w3schools.com/lib/w3.css'>
+    <link href='https://fonts.googleapis.com/css?family=Pacifico|Open+Sans' rel='stylesheet'>
+    <link rel='stylesheet' href='assets/w3.css'>
     <link rel='stylesheet' href='assets/main.css'>
     <script src="assets/jquery-3.1.1.min.js"></script>
     <script src="assets/main.js"></script>
